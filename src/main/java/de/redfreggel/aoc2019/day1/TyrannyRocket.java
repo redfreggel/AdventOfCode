f2e588cc6ca40e898232b94c996cf01d09871466
Antje <antje.frecke@ikea.com>
2019-12-06 15:48:52 +0100

    Added and Solved Riddle 2 Day 1

--- src/main/java/de/redfreggel/aoc2019/day1/TyrannyRocket.java
+++ src/main/java/de/redfreggel/aoc2019/day1/TyrannyRocket.java
@@ -9,12 +9,6 @@ public class TyrannyRocket {
 
     public static void main(String[] args) throws IOException {
         TyrannyRocket rocket = new TyrannyRocket();
-        System.out.println("Calculation for mass= 12: "+ rocket.calculateModuleConsumption(12)+ " and should be 2");
-        System.out.println("Calculation for mass= 14: "+ rocket.calculateModuleConsumption(14)+ " and should be 2");
-        System.out.println("Calculation for mass= 1969: "+ rocket.calculateModuleConsumption(1969)+ " and should be 654");
-        System.out.println("Calculation for mass= 100756: "+ rocket.calculateModuleConsumption(100756)+ " and should be 33583");
-
-        System.out.println("------------- everything fine so far ---------------------");
         List<Integer> list = new ArrayList<Integer>();
         try{
             File file = new File("C:\\temp\\InteliJ\\AdventOfCode\\src\\main\\resources\\inputDay1_1.txt");
@@ -42,17 +36,48 @@ public class TyrannyRocket {
         }catch (Exception e){
             e.printStackTrace();
         }
+
+       // rocket.solveRiddle1(list);
+        rocket.solveRiddle2(list);
+    }
+
+    public void solveRiddle2(List<Integer> pInput){
         int calculatedMass = 0;
-        if(list.size()>0){
-            for (Integer moduleMass: list)   {
-                calculatedMass += rocket.calculateModuleConsumption(moduleMass);
+        if(pInput.size()>0){
+            for (Integer moduleMass: pInput)   {
+
+                int calculatedModuleMass = this.calculateModuleConsumption(moduleMass);
+                while (calculatedModuleMass>0){
+                    calculatedMass +=calculatedModuleMass;
+                    calculatedModuleMass = this.calculateModuleConsumption(calculatedModuleMass);
+                }
+
             }
         }
 
         System.out.println("Calculation = "+calculatedMass);
     }
 
-    public int calculateModuleConsumption(int pMass){
+
+    public void solveRiddle1(List<Integer> pInput){
+        System.out.println("Calculation for mass= 12: "+ this.calculateModuleConsumption(12)+ " and should be 2");
+        System.out.println("Calculation for mass= 14: "+ this.calculateModuleConsumption(14)+ " and should be 2");
+        System.out.println("Calculation for mass= 1969: "+ this.calculateModuleConsumption(1969)+ " and should be 654");
+        System.out.println("Calculation for mass= 100756: "+ this.calculateModuleConsumption(100756)+ " and should be 33583");
+
+        System.out.println("------------- everything fine so far ---------------------");
+
+        int calculatedMass = 0;
+        if(pInput.size()>0){
+            for (Integer moduleMass: pInput)   {
+                calculatedMass += this.calculateModuleConsumption(moduleMass);
+            }
+        }
+
+        System.out.println("Calculation = "+calculatedMass);
+    }
+
+    private int calculateModuleConsumption(int pMass){
         double consumption = Math.ceil(pMass/3)-2;
         return (int) consumption;
 
