6d0d43008150b19fcd7cbf6eec2e44e09164f621
Antje Frecke <antje.frecke@googlemail.com>
2019-12-10 19:42:11 +0100

    Finalized challenge 1 - day 4

--- src/main/java/de/redfreggel/aoc2019/day4/SecureContainer.java
+++ src/main/java/de/redfreggel/aoc2019/day4/SecureContainer.java
@@ -4,15 +4,15 @@ import java.util.regex.Pattern;
 
 public class SecureContainer {
 
-    public final static String regEx1 = "11.+";
-    public final static String regEx2 = ".+22.+";
-    public final static String regEx3 = ".+33.+";
-    public final static String regEx4 = ".+44.+";
-    public final static String regEx5 = ".+55.+";
-    public final static String regEx6 = ".+66.+";
-    public final static String regEx7 = ".+77.+";
-    public final static String regEx8 = ".+88.+";
-    public final static String regEx9 = ".+99.+";
+    public final static String regEx1 = ".*11.*";
+    public final static String regEx2 = ".*22.*";
+    public final static String regEx3 = ".*33.*";
+    public final static String regEx4 = ".*44.*";
+    public final static String regEx5 = ".*55.*";
+    public final static String regEx6 = ".*66.*";
+    public final static String regEx7 = ".*77.*";
+    public final static String regEx8 = ".*88.*";
+    public final static String regEx9 = ".*99.*";
 
     public final static int inputStart = 152085;
     public final static int inputEnd =  670283;
@@ -21,6 +21,8 @@ public class SecureContainer {
         int matches = 0;
         for(int i = inputStart; i<=inputEnd; i++){
             String tocheck = Integer.toString(i);
+
+
                if (Pattern.matches(regEx1, tocheck)
                      || Pattern.matches(regEx2, tocheck)
                     || Pattern.matches(regEx3, tocheck)
@@ -39,11 +41,10 @@ public class SecureContainer {
                 int fifthDigit = (i-((firstDigit*100000)+(secondDigit*10000)+(thirdDigit*1000)+(forthDigit*100)))/10;
                 int sixDigit =   (i-((firstDigit*100000)+(secondDigit*10000)+(thirdDigit*1000)+(forthDigit*100)+(fifthDigit*10)))/1;
 
-                if((firstDigit<= secondDigit)&&(secondDigit<=thirdDigit)&& (thirdDigit<=forthDigit)&&(forthDigit<=fifthDigit) &&(fifthDigit<=sixDigit)){
-                    System.out.println(i);
+                if((firstDigit<= secondDigit)&&(secondDigit<=thirdDigit)&& (thirdDigit<=forthDigit)&&(forthDigit<=fifthDigit) &&(fifthDigit<=sixDigit)) {
+                   // System.out.println(i);
                     matches++;
                 }
-
              }
         }
         System.out.println(matches);
