bcb7c85181557ef8d69e5610e3a16d219a5560e4
Antje Frecke <antje.frecke@googlemail.com>
2020-01-04 10:05:16 +0100

    Simplified day1

--- src/main/java/de/redfreggel/aoc2019/day1/TyrannyRocket.java
+++ src/main/java/de/redfreggel/aoc2019/day1/TyrannyRocket.java
@@ -2,6 +2,9 @@ package de.redfreggel.aoc2019.day1;
 
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,75 +13,60 @@ public class TyrannyRocket {
     public static void main(String[] args) throws IOException {
         TyrannyRocket rocket = new TyrannyRocket();
         List<Integer> list = new ArrayList<Integer>();
-        try{
-            File file = new File("C:\\temp\\InteliJ\\AdventOfCode\\src\\main\\resources\\inputDay1_1.txt");
-            BufferedReader reader = null;
+        Path path = Paths.get(".\\src\\main\\resources\\inputDay1_1.txt");
 
-            try {
-                reader = new BufferedReader(new FileReader(file));
-                String text = null;
+        try (BufferedReader reader = Files.newBufferedReader(path)) {
+            String text = null;
 
-                while ((text = reader.readLine()) != null) {
-                    list.add(Integer.parseInt(text));
-                }
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                try {
-                    if (reader != null) {
-                        reader.close();
-                    }
-                } catch (IOException e) {
-                }
+            while ((text = reader.readLine()) != null) {
+                list.add(Integer.parseInt(text));
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
-       // rocket.solveRiddle1(list);
+        // rocket.solveRiddle1(list);
         rocket.solveRiddle2(list);
     }
 
-    public void solveRiddle2(List<Integer> pInput){
+    public void solveRiddle2(List<Integer> pInput) {
         int calculatedMass = 0;
-        if(pInput.size()>0){
-            for (Integer moduleMass: pInput)   {
+        if (pInput.size() > 0) {
+            for (Integer moduleMass : pInput) {
 
                 int calculatedModuleMass = this.calculateModuleConsumption(moduleMass);
-                while (calculatedModuleMass>0){
-                    calculatedMass +=calculatedModuleMass;
+                while (calculatedModuleMass > 0) {
+                    calculatedMass += calculatedModuleMass;
                     calculatedModuleMass = this.calculateModuleConsumption(calculatedModuleMass);
                 }
 
             }
         }
 
-        System.out.println("Calculation = "+calculatedMass);
+        System.out.println("Calculation = " + calculatedMass);
     }
 
 
-    public void solveRiddle1(List<Integer> pInput){
-        System.out.println("Calculation for mass= 12: "+ this.calculateModuleConsumption(12)+ " and should be 2");
-        System.out.println("Calculation for mass= 14: "+ this.calculateModuleConsumption(14)+ " and should be 2");
-        System.out.println("Calculation for mass= 1969: "+ this.calculateModuleConsumption(1969)+ " and should be 654");
-        System.out.println("Calculation for mass= 100756: "+ this.calculateModuleConsumption(100756)+ " and should be 33583");
+    public void solveRiddle1(List<Integer> pInput) {
+        System.out.println("Calculation for mass= 12: " + this.calculateModuleConsumption(12) + " and should be 2");
+        System.out.println("Calculation for mass= 14: " + this.calculateModuleConsumption(14) + " and should be 2");
+        System.out.println("Calculation for mass= 1969: " + this.calculateModuleConsumption(1969) + " and should be 654");
+        System.out.println("Calculation for mass= 100756: " + this.calculateModuleConsumption(100756) + " and should be 33583");
 
         System.out.println("------------- everything fine so far ---------------------");
 
         int calculatedMass = 0;
-        if(pInput.size()>0){
-            for (Integer moduleMass: pInput)   {
+        if (pInput.size() > 0) {
+            for (Integer moduleMass : pInput) {
                 calculatedMass += this.calculateModuleConsumption(moduleMass);
             }
         }
 
-        System.out.println("Calculation = "+calculatedMass);
+        System.out.println("Calculation = " + calculatedMass);
     }
 
-    private int calculateModuleConsumption(int pMass){
-        double consumption = Math.ceil(pMass/3)-2;
+    private int calculateModuleConsumption(int pMass) {
+        double consumption = Math.ceil(pMass / 3) - 2;
         return (int) consumption;
 
     }
