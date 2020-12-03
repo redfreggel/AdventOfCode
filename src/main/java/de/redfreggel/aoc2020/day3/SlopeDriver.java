8f4666c021a81fbdf6d6eef91d6ef1971bd99bcd
Antje <antje.frecke@ikea.com>
2020-12-03 11:24:45 +0100

    AOC Day 3 - finally - and again to complicated

--- src/main/java/de/redfreggel/aoc2020/day3/SlopeDriver.java
+++ src/main/java/de/redfreggel/aoc2020/day3/SlopeDriver.java
@@ -1,4 +1,76 @@
 package de.redfreggel.aoc2020.day3;
 
+import java.io.BufferedReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SlopeDriver {
+
+    public static final char tree = '#';
+
+    /* Task 2:
+        Right 1, down 1.
+        Right 3, down 1. (This is the slope you already checked.)
+        Right 5, down 1.
+        Right 7, down 1.
+        Right 1, down 2.
+     */
+
+    public static void main(String[] args) {
+        Path path = Paths.get(".\\src\\main\\resources\\aoc2020\\inputDay3_2020_task_1.txt");
+        List<SloppySlope> sloppySlopes = new ArrayList<>();
+        sloppySlopes.add(new SloppySlope(1,1));
+        sloppySlopes.add(new SloppySlope(3,1));
+        sloppySlopes.add(new SloppySlope(5,1));
+        sloppySlopes.add(new SloppySlope(7,1));
+        sloppySlopes.add(new SloppySlope(1,2));
+
+        /*
+        Task 1
+        Starting at the top-left corner of your map and following a slope of right 3 and down 1.
+        How many trees would you encounter?
+         */
+        int slopeDown = 0;
+
+        try (BufferedReader reader = Files.newBufferedReader(path)) {
+            String fileText;
+
+            do{
+                fileText = reader.readLine();
+                StringBuilder treeLine;
+                if(slopeDown== 0){
+                    fileText = reader.readLine();
+                }
+                slopeDown++;
+                for(SloppySlope slope : sloppySlopes){
+                    slope.moveDownByOne();
+                    if(!slope.hasToCheck()){
+                        continue;
+                    }
+                    slope.moveRight();
+                    if(fileText != null) {//not at the end
+                        treeLine = new StringBuilder(fileText);
+                        while( treeLine.length() < slope.getBufferedRightPosition()){
+                            treeLine.append(fileText);
+                        }
+
+                        if (treeLine.charAt(slope.getActualRightPosition()) == tree) slope.hitByTree();
+                    }
+                }
+            }while(fileText !=  null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        long result = 1;
+        for(SloppySlope slope : sloppySlopes){
+            result = result*slope.getHitByTreeCounter();
+            System.out.println("Hit by trees per slope: "+slope.getHitByTreeCounter());
+        }
+        System.out.println("RiddleResult = "+result);
+    }
+
 }
---
+++ src/main/java/de/redfreggel/aoc2020/day3/SloppySlope.java
@@ -0,0 +1,43 @@
+package de.redfreggel.aoc2020.day3;
+
+public class SloppySlope {
+
+    private final int posToMoveRight;
+    private final int posToMoveDown;
+    private int hitByTreeCounter= 0;
+    private int actualPositionRight =0;
+    private int actualPositionLine =0;
+
+    public SloppySlope(int pPosToMoveRight, int pPosToMoveDown){
+        this.posToMoveDown = pPosToMoveDown;
+        this.posToMoveRight = pPosToMoveRight;
+    }
+
+    public void hitByTree(){
+        hitByTreeCounter++;
+    }
+
+    public void moveRight(){
+        actualPositionRight = actualPositionRight+posToMoveRight;
+    }
+
+    public void moveDownByOne(){
+        actualPositionLine ++;
+    }
+
+    public int getActualRightPosition(){
+        return actualPositionRight;
+    }
+
+    public int getHitByTreeCounter(){
+        return this.hitByTreeCounter;
+    }
+
+    public boolean hasToCheck(){
+        return this.actualPositionLine%this.posToMoveDown==0;
+    }
+
+    public int getBufferedRightPosition() {
+        return  this.getActualRightPosition()+posToMoveRight;
+    }
+}
